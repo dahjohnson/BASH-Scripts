88217e2c1164f4e1d4cc39aa2f3f24b7a180bd2e
dahjohnson <djohnson@devopslabs4me.com>
2022-10-29 18:35:03 -0400

    modified addnewuser.sh script to create new users by accepting arguments

--- addnewuser.sh
+++ addnewuser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Add new user script
-
+:
 #Checking for Root/sudo privileges
 if [ "$EUID" -ne 0 ]
 then 
@@ -9,6 +9,32 @@ then
         echo "Permission Error: Elevate permissions using sudo"
 	echo "Example: \"sudo bash <path_to_script>\" 🤓"
         echo "====================================="
+	exit
+fi
+
+#If argument provided, create new user(s) and skip the rest of BASH script
+if
+	[ $# -ge 1 ] && id -u $@ &>/dev/null;
+then
+	echo "=====================================" 
+	echo "User(s) $@ already exists!!!"
+	echo "====================================="
+	echo "$(id $@)"
+        echo
+      	exit	
+elif
+	
+	[ $# -ge 1 ]
+then
+	echo
+	for user in $@;
+	do
+		useradd -m $user;
+		echo -e "Username:$user\thome_dir:$(getent passwd $user | cut -d: -f6)";
+	done
+	echo
+	echo "You have successfully created your new user(s) 👍";
+	echo
 	exit
 fi
 
@@ -24,7 +50,7 @@ then
 	echo "======================="
 	exit
 else
-	useradd -m $user_name 2> /dev/null
+	useradd -m $user_name
 	echo 
 	echo "User $user_name has been created and their home dir is $(getent passwd $user_name | cut -d: -f6)👍"
 	echo 
